f6832a7fe64d7a46d30a91e3d66a4559d85a6aa3
Benjamin Armintor <armintor@gmail.com>
2015-03-25 13:55:06 -0400

    stub out some indexing for GenericWork MODS

--- app/models/concerns/cul/ac3/datastreams/desc_metadata.rb
+++ app/models/concerns/cul/ac3/datastreams/desc_metadata.rb
@@ -1,9 +1,83 @@
-class Cul::Ac3::Datastreams::DescMetadata < ::ActiveFedora::Datastream
+class Cul::Ac3::Datastreams::DescMetadata < ::ActiveFedora::File
   include ::ActiveFedora::Datastreams::NokogiriDatastreams
+  MODSNS = {mods:'http://www.loc.gov/mods/v3'}
   def self.xml_template
     Nokogiri::XML::Document.parse("<mods xmlns='http://www.loc.gov/mods/v3' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-5.xsd' version='3.5'>")
   end
   def to_solr(solr_doc={}, opts={})
+    keys = [:resource_type, :title, :creator, :contributor, :description, :tag, :rights,
+       :publisher, :date_created, :subject, :language, :identifier, :based_near, :related_url]
+
+    keys.each do |key|
+      fname = "#{key}_tesim" # + solr_name(key.to_s, :stored_searchable)
+      solr_doc[fname] ||= []
+      if (vals = self.send(key))
+        if Array === vals
+          solr_doc[fname] += vals
+        else
+          solr_doc[fname] = vals
+        end
+      end
+    end
     solr_doc
+  end
+
+  def resource_type
+    ng_xml.xpath('mods:mods/mods:genre', MODSNS).map {|n| n.text}
+  end
+  def title
+    ng_xml.xpath('mods:mods/mods:titleInfo', MODSNS).map {|n| n.text.strip}.first
+  end
+  def label
+    title.first
+  end
+  def creator
+    ng_xml.xpath("mods:mods/mods:name/mods:role/mods:roleTerm[normalize-space(.)='author']", MODSNS).collect do|r|
+      name_parts = []
+      r.parent.parent.xpath('mods:namePart',MODSNS).each {|p| name_parts << p.text }
+      name_parts.join(' ')
+    end
+  end
+  def contributor
+    []
+  end
+  def description
+    ng_xml.xpath('mods:mods/mods:abstract', MODSNS).map {|n| n.text}
+  end
+  def tag
+    []
+  end
+  def rights
+    []
+  end
+  def publisher
+    []
+  end
+  def date_created
+    ng_xml.xpath('mods:mods/mods:originInfo/mods:dateIssued', MODSNS).map {|n| n.text}
+  end
+  def subject
+    ng_xml.xpath('mods:mods/mods:subject', MODSNS).map {|n| n.text}
+  end
+  def language
+    ng_xml.xpath('mods:mods/mods:language', MODSNS).map {|n| n.text}
+  end
+  def identifier
+    ng_xml.xpath('mods:mods/mods:identifier', MODSNS).map {|n| n.text}
+  end
+  def based_near
+    []
+  end
+  def related_url
+    ng_xml.xpath("mods:mods/mods:identifier[@type='CDRS doi']", MODSNS).map {|n| n.text}
+  end
+  def full_text
+    FullText.new(ng_xml.text)
+  end
+  class FullText
+    attr_reader :content
+    def initialize(content)
+      @content = content
+    end
   end
 end
--- app/models/generic_work.rb
+++ app/models/generic_work.rb
@@ -1,4 +1,24 @@
 class GenericWork < ActiveFedora::Base
   include Sufia::GenericWork
-  has_metadata :name => "descMetadata", :type=> Cul::Ac3::Datastreams::DescMetadata
+
+  has_metadata 'descMetadata', :type=> Cul::Ac3::Datastreams::DescMetadata
+
+  delegate :label, to: :descMetadata
+  delegate :title, to: :descMetadata
+  delegate :date_created, to: :descMetadata
+  delegate :full_text, to: :descMetadata
+  delegate :resource_type, to: :descMetadata
+  delegate :related_url, to: :descMetadata
+  delegate :language, to: :descMetadata
+  delegate :subject, to: :descMetadata
+  delegate :creator, to: :descMetadata
+
+  property :identifier, predicate: ::RDF::DC.identifier do |index|
+    index.as :stored_searchable
+  end
+
+  property :date_uploaded, predicate: ::RDF::DC.dateSubmitted, multiple: false do |index|
+    index.type :date
+    index.as :stored_sortable
+  end
 end
